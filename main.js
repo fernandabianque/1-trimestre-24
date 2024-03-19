0c32e07d2c0b0b0f815ca34a3870e3ba60e0c117
fernandabianque <105672395+fernandabianque@users.noreply.github.com>
2024-03-19 09:52:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -38,13 +38,18 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
-        return "Prazo Finalizado";
+        return[0,0,0,0];
+
     }
 }
 
 function atualizaCronometro(){
+    document.getElementById("dias0").calculaTempo(tempos [0]) [0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[0]) [1];
+        document.getElementById("min0").textContent = calculaTempo(tempos[0]) [2];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[0]) [3];
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+       // contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
 
@@ -53,4 +58,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-//comecaCronometro();
+comecaCronometro();
